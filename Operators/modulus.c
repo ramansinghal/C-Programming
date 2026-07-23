63ffe2b07c8f14bf413f474d1cd465f10a7940ad
Ramansinghal <ramansinghal98976@gmail.com>
2026-07-23 15:40:00 +0530

    modify modulus

--- Operators/modulus.c
+++ Operators/modulus.c
@@ -1,16 +1,11 @@
-// #include<stdio.h>
-// int main() {
-//     int a,b;
-//     printf("a = ");
-//     scanf("%d",&a);
-//     printf("b = ");
-//     scanf("%d",&b);
-//     printf("modulus is %d",a%b);
-//     return 0;
-// }
-
 #include<stdio.h>
 int main() {
-    printf("%d",4*9/2*7);
+    int a,b;
+    printf("a = ");
+    scanf("%d",&a);
+    printf("b = ");
+    scanf("%d",&b);
+    printf("modulus is %d",a%b);
     return 0;
 }
+
